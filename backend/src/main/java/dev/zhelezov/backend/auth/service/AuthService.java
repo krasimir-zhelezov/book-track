ee2574d564ad7eb9a0c08d4a4f8a005142b12105
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-07 10:09:33 +0300

    fix(backend): correct password matching logic

--- backend/src/main/java/dev/zhelezov/backend/auth/service/AuthService.java
+++ backend/src/main/java/dev/zhelezov/backend/auth/service/AuthService.java
@@ -29,7 +29,7 @@ public class AuthService {
     private final JwtUtil jwtUtil;
 
     public UserDto signUp(SignUpDto signUpDto) {
-        if (signUpDto.getPassword1() != signUpDto.getPassword2()) {
+        if (!signUpDto.getPassword1().equals(signUpDto.getPassword2())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Passwords do not match");
         }
         User user = userRepository.save(new User(signUpDto.getEmail(), passwordEncoder.encode(signUpDto.getPassword1())));
