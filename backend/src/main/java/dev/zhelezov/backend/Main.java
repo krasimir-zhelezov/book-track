956462c882a0f502bd3d56092a45e17763b00b63
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-08 20:58:43 +0300

    feat(backend): default admin account at start

--- backend/src/main/java/dev/zhelezov/backend/Main.java
+++ backend/src/main/java/dev/zhelezov/backend/Main.java
@@ -1,7 +1,16 @@
 package dev.zhelezov.backend;
 
+import java.util.Set;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import dev.zhelezov.backend.auth.model.Role;
+import dev.zhelezov.backend.auth.model.User;
+import dev.zhelezov.backend.auth.repository.UserRepository;
 
 @SpringBootApplication
 public class Main {
@@ -10,4 +19,14 @@ public class Main {
 		SpringApplication.run(Main.class, args);
 	}
 
+	@Bean
+    public CommandLineRunner initAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.findByEmail("admin@example.com").isEmpty()) {
+                User admin = new User("admin@example.com", passwordEncoder.encode("admin123"), Role.ADMIN);
+                userRepository.save(admin);
+                System.out.println("Created default admin account");
+            }
+        };
+    }
 }
