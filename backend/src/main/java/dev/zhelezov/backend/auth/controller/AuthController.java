c4614da7dcf421824b08099ea10a391cfe36ff5d
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-12 20:04:36 +0300

    docs(backend): documentation for authentication api

--- backend/src/main/java/dev/zhelezov/backend/auth/controller/AuthController.java
+++ backend/src/main/java/dev/zhelezov/backend/auth/controller/AuthController.java
@@ -21,31 +21,45 @@ import dev.zhelezov.backend.auth.dto.SignInDto;
 import dev.zhelezov.backend.auth.dto.SignUpDto;
 import dev.zhelezov.backend.auth.dto.UserDto;
 import dev.zhelezov.backend.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/auth")
+@Tag(name = "Authentication Management", description = "Endpoints for managing authentication")
 public class AuthController {
 
     private final AuthService authService;
     
     @SecurityRequirements()
     @PostMapping("/sign-in")
+    @Operation(summary = "Sign in", description = "Authenticate an existing user and return user details with JWT token")
+    @ApiResponse(responseCode = "200", description = "Authentication successful")
+    @ApiResponse(responseCode = "401", description = "Invalid credentials", content = @Content())
     public ResponseEntity<?> signIn(@RequestBody SignInDto signInDto) {
         return handleAuthRequest(() -> authService.signIn(signInDto));
     }
 
     @SecurityRequirements()
     @PostMapping("/sign-up")
+    @Operation(summary = "Sign up", description = "Register a new user, authenticate them, and return user details with JWT token")
+    @ApiResponse(responseCode = "200", description = "Registration and authentication successful")
+    @ApiResponse(responseCode = "400", description = "Bad request (password mismatch, user already exists)", content = @Content())
     public ResponseEntity<?> signUp(@RequestBody SignUpDto signUpDto) {
         return handleAuthRequest(() -> authService.signUp(signUpDto));
     }
 
 
     @GetMapping("/profile")
+    @Operation(summary = "User details", description = "Returns the authenticated user's details")
+    @ApiResponse(responseCode = "200", description = "User details found")
+    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content())
     public ResponseEntity<?> profile() {
         return handleAuthRequest(() -> authService.profile());
     }
@@ -58,21 +72,11 @@ public class AuthController {
         }
     }
 
-    @GetMapping("/debug")
-    public Map<String, Object> checkAuth() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        return Map.of(
-            "name", auth.getName(),
-            "authorities", auth.getAuthorities().stream()
-                            .map(GrantedAuthority::getAuthority)
-                            .collect(Collectors.toList()),
-            "credentials", auth.getCredentials(),
-            "details", auth.getDetails()
-        );
-    }
-
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("/users")
+    @Operation(summary = "Get all users", description = "Returns a list of all users if the user has admin role")
+    @ApiResponse(responseCode = "200", description = "Users found")
+    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content())
     public ResponseEntity<List<UserDto>> getUsers() {
         return ResponseEntity.ok().body(authService.getUsers());
     }
