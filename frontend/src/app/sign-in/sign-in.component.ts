5beee639e202bae73662abbddd7d37ac8d385d89
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-13 11:10:54 +0300

    feat(frontend): sign in form

--- frontend/src/app/sign-in/sign-in.component.ts
+++ frontend/src/app/sign-in/sign-in.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
-
+  email: string = '';
+  password: string = '';
 }
