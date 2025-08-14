47461f378e7068a1d02be814b95ce6d18f701fe0
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-14 20:28:45 +0300

    feat(frontend): button component

--- frontend/src/app/sign-in/sign-in.component.ts
+++ frontend/src/app/sign-in/sign-in.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputComponent } from '../shared/components/input/input.component';
 import { LabelComponent } from '../shared/components/label/label.component';
+import { ButtonComponent } from '../shared/components/button/button.component';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [FormsModule, InputComponent, LabelComponent ],
+  imports: [FormsModule, InputComponent, LabelComponent, ButtonComponent ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
