903e3b45821e0de66644a8f9076b231409424cf9
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-14 20:07:23 +0300

    feat(frontend): input component

--- frontend/src/app/sign-in/sign-in.component.ts
+++ frontend/src/app/sign-in/sign-in.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InputComponent } from '../shared/components/input/input.component';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [FormsModule],
+  imports: [FormsModule, InputComponent ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
