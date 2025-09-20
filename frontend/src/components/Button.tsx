2bd40012d2f8730edb77545e49c857d8def439b2
krasimir-zhelezov <krasizh07@gmail.com>
2025-09-20 20:19:24 +0300

    feat(frontend): on click function for button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,7 +1,15 @@
 interface ButtonProps {
   children: React.ReactNode;
+  onClick?: () => void;
 }
 
-export default function Button ({ children }: ButtonProps) {
-    return <button className="bg-blue-500 hover:bg-blue-600 hover:cursor-pointer text-white font-bold py-2 px-4 rounded">{children}</button>
+export default function Button ({ children, onClick }: ButtonProps) {
+    return (
+        <button
+            onClick={onClick}
+            className="bg-blue-500 hover:bg-blue-600 hover:cursor-pointer text-white font-bold py-2 px-4 rounded"
+        >
+            {children}
+        </button>
+    )
 }
