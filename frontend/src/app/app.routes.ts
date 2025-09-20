edbe6caf34799ba7a5b664794c184cd95cc533d3
krasimir-zhelezov <krasizh07@gmail.com>
2025-09-20 19:33:16 +0300

    feat: replace angular with react

--- frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { SignInComponent } from './sign-in/sign-in.component';
-
-export const routes: Routes = [
-    { path: 'auth/sign-in', component: SignInComponent },
-];
