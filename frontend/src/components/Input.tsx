99c4c946fefe1164873621d6f3cbc0f9122fbe68
krasimir-zhelezov <krasizh07@gmail.com>
2025-09-21 17:36:34 +0300

    fix(frontend): remove outline from input

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -4,5 +4,5 @@ interface InputProps {
 }
 
 export default function Input({ type = "text", placeholder="" }: InputProps) {
-    return <input className="border border-gray-300 rounded-md px-4 py-2 shadow-md hover:shadow-lg" type={type} placeholder={placeholder}/>
+    return <input className="border border-gray-300 rounded-md px-4 py-2 shadow-md hover:shadow-lg focus:outline-none" type={type} placeholder={placeholder}/>
 }
